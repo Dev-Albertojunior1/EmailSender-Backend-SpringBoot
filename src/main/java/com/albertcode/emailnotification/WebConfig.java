4d592d4c8200baa9c67e7cc3a5c04d3723251f35
studentSystem <albertojunior848423803@gmail.com>
2024-06-14 20:07:59 -0700

    commit

---
+++ src/main/java/com/albertcode/emailnotification/WebConfig.java
@@ -0,0 +1,18 @@
+package com.albertcode.emailnotification;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Permitir acesso apenas do frontend React em desenvolvimento
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
