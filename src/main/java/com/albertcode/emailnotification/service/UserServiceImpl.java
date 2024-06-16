98315af9531ce3581ec8092c6029f3e3baf49915
studentSystem <albertojunior848423803@gmail.com>
2024-06-15 18:19:34 -0700

    commit

--- src/main/java/com/albertcode/emailnotification/service/UserServiceImpl.java
+++ src/main/java/com/albertcode/emailnotification/service/UserServiceImpl.java
@@ -33,7 +33,7 @@ public class UserServiceImpl implements UserService{
         User user = UserMapper.mapToUser(new User(),requestDto);
 //        user.setPassword(""); // Define a senha como uma string vazia
         emailService.sendEmail(EmailDetails.builder()
-                        .messageBody("Registration Successful with mail id: "+requestDto.getEmail())
+                        .messageBody("You’ve successfully subscribed! with mail id: "+requestDto.getEmail() + "Stay tuned for updates and exclusive content.")
                         .recipient(requestDto.getEmail())
                         .subject("REGISTRATION SUCCESS")
                 .build());
